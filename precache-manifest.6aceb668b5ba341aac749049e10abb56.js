f855ca2d24c33bb447fab1520af999f7a4a01530
Lorenze Jay <Lorenzejay@gmail.com>
2020-08-28 19:39:11 -0700

    Updates

---
+++ precache-manifest.6aceb668b5ba341aac749049e10abb56.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99c81fbeed671dc0bc4032c98287e873",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "65612a7cddf6f4d77ef2",
+    "url": "/portfolio/static/css/main.df7774a2.chunk.css"
+  },
+  {
+    "revision": "00ceddee80e14b2d846d",
+    "url": "/portfolio/static/js/2.fcf7e8fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.fcf7e8fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65612a7cddf6f4d77ef2",
+    "url": "/portfolio/static/js/main.afcf0e7f.chunk.js"
+  },
+  {
+    "revision": "1cd67412032333bf4ae3",
+    "url": "/portfolio/static/js/runtime-main.9a9f7afd.js"
+  },
+  {
+    "revision": "f38b6dc8a51a5626473f9d31bad7becf",
+    "url": "/portfolio/static/media/DSC02193.f38b6dc8.jpg"
+  },
+  {
+    "revision": "6c5f1f379625c50cbdde09b0c4ebe018",
+    "url": "/portfolio/static/media/Entry-Dark-App.6c5f1f37.png"
+  },
+  {
+    "revision": "969d268eb164f3a86c1436df8869bcce",
+    "url": "/portfolio/static/media/bmi-calculator-app.969d268e.png"
+  },
+  {
+    "revision": "9e840087345df3eca985ef5d17bfa37f",
+    "url": "/portfolio/static/media/screencapture-file-C-Users-HP-Desktop-Personal-Projects-covid-19-API-index-html-2020-07-28-21_42_43.9e840087.png"
+  },
+  {
+    "revision": "e96109f0d37103df508312817ee2bf96",
+    "url": "/portfolio/static/media/screencapture-lorenzejay-github-io-todo-list-2020-07-28-21_26_22.e96109f0.png"
+  }
+]);
