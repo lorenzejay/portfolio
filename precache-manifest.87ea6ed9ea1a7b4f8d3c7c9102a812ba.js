bbda96b78a87f15a7e2622481dfe9c57d42097d0
Lorenze Jay <Lorenzejay@gmail.com>
2020-09-08 05:13:55 -0700

    Updates

---
+++ precache-manifest.87ea6ed9ea1a7b4f8d3c7c9102a812ba.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3550c1da85d0b81f5f2242bec38282c2",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "562724a55ccd4c49a546",
+    "url": "/portfolio/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "5a16479d14ce80a40ff2",
+    "url": "/portfolio/static/css/main.c7af94ff.chunk.css"
+  },
+  {
+    "revision": "562724a55ccd4c49a546",
+    "url": "/portfolio/static/js/2.94391d63.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.94391d63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a16479d14ce80a40ff2",
+    "url": "/portfolio/static/js/main.17972f62.chunk.js"
+  },
+  {
+    "revision": "1cd67412032333bf4ae3",
+    "url": "/portfolio/static/js/runtime-main.9a9f7afd.js"
+  },
+  {
+    "revision": "f38b6dc8a51a5626473f9d31bad7becf",
+    "url": "/portfolio/static/media/DSC02193.f38b6dc8.jpg"
+  },
+  {
+    "revision": "6c5f1f379625c50cbdde09b0c4ebe018",
+    "url": "/portfolio/static/media/Entry-Dark-App.6c5f1f37.png"
+  },
+  {
+    "revision": "969d268eb164f3a86c1436df8869bcce",
+    "url": "/portfolio/static/media/bmi-calculator-app.969d268e.png"
+  },
+  {
+    "revision": "e56e37dea453afc79e0a029d0c615488",
+    "url": "/portfolio/static/media/js.e56e37de.png"
+  },
+  {
+    "revision": "a02731bffbaa4f0d6109ed40548a107a",
+    "url": "/portfolio/static/media/lorenze-portfile.a02731bf.jpg"
+  },
+  {
+    "revision": "55e83371dbf50a5f027b1fe31a054ad3",
+    "url": "/portfolio/static/media/nodejs.55e83371.png"
+  },
+  {
+    "revision": "f297dfcd8b3e9215ee862f232d5c9d13",
+    "url": "/portfolio/static/media/react.f297dfcd.png"
+  },
+  {
+    "revision": "a2bc8ecf6d9052818b36402f0431c60f",
+    "url": "/portfolio/static/media/sass.a2bc8ecf.png"
+  },
+  {
+    "revision": "9e840087345df3eca985ef5d17bfa37f",
+    "url": "/portfolio/static/media/screencapture-file-C-Users-HP-Desktop-Personal-Projects-covid-19-API-index-html-2020-07-28-21_42_43.9e840087.png"
+  },
+  {
+    "revision": "e96109f0d37103df508312817ee2bf96",
+    "url": "/portfolio/static/media/screencapture-lorenzejay-github-io-todo-list-2020-07-28-21_26_22.e96109f0.png"
+  },
+  {
+    "revision": "a6a8b49225afcfae5995c7b7b17147af",
+    "url": "/portfolio/static/media/weather-application.a6a8b492.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.6aceb668b5ba341aac749049e10abb56.js"
+  "/portfolio/precache-manifest.87ea6ed9ea1a7b4f8d3c7c9102a812ba.js"
 );
 
 self.addEventListener('message', (event) => {
